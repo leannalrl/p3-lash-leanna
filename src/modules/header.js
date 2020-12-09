f202f2b7209077dcfca285d680a88ce0febc8248
Leanna <lrlash17@ufl.edu>
2020-12-08 21:48:07 -0500

    added code to header

--- src/modules/header.js
+++ src/modules/header.js
@@ -2,6 +2,7 @@ class Header{
   constructor(title){
     this.title = title;
   }
+
 }
 
-export const header = new Header('ES6 App');
+export const header = new Header('Christmas Lunch Generator');
