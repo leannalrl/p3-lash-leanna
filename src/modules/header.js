33bd1cbcd8b100ca82ebc1590fdaf9794f4e3f61
Leanna <lrlash17@ufl.edu>
2020-12-09 19:59:00 -0500

    added an alert to the header for second function

--- src/modules/header.js
+++ src/modules/header.js
@@ -5,3 +5,16 @@ class Header{
 }
 
 export const header = new Header('Holidays can be stressful, so let your generator decided you lunch for you!');
+
+class Introduction{
+  constuctor(hello_you){
+    this.hello_you = hello_you;
+  }
+}
+
+function showMessage() {
+  alert( 'Welcom to the Christmas Lunch Generator. To continue click ok twice.' );
+}
+
+let hello_you = showMessage();
+export const title = showMessage();
